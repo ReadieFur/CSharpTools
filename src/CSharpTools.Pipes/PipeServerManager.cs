15552b67771e4955f7bf991ee03027364af62cdd
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2022-08-14 21:27:23 +0100

    Update PipeServerManager.cs

--- src/CSharpTools.Pipes/PipeServerManager.cs
+++ src/CSharpTools.Pipes/PipeServerManager.cs
@@ -12,6 +12,7 @@ namespace CSharpTools.Pipes
         private readonly object lockObject = new object();
         private Mutex mutex;
         private readonly ConcurrentDictionary<Guid, PipeServer> pipeServers = new ConcurrentDictionary<Guid, PipeServer>();
+        private PipeServer? pendingPipeServer;
         private readonly int bufferSize;
         private readonly int maxAllowedServerInstances;
         
@@ -40,6 +41,7 @@ namespace CSharpTools.Pipes
             lock (lockObject)
             {
                 if (isDisposed) return;
+                if (pendingPipeServer != null) pendingPipeServer.Dispose();
                 foreach (PipeServer pipeServer in pipeServers.Values) pipeServer.Dispose();
                 mutex.Dispose();
                 isDisposed = true;
@@ -50,41 +52,61 @@ namespace CSharpTools.Pipes
         {
             lock (lockObject)
             {
-                if (isDisposed) return;
-
-                if (maxAllowedServerInstances > 0 && pipeServers.Count > maxAllowedServerInstances)
+                if (isDisposed || pendingPipeServer != null) return;
+                else if (maxAllowedServerInstances > 0 && pipeServers.Count > maxAllowedServerInstances)
                     throw new IOException("The maximum number of server instances has been exceeded.");
-
-                //Generate a unique ID for the new pipe.
-                Guid guid;
-                do guid = Guid.NewGuid();
-                while (pipeServers.ContainsKey(guid));
 
                 //Create the new pipe.
                 PipeServer pipeServer = new(ipcName, bufferSize, maxAllowedServerInstances);
-                pipeServer.OnConnect += () =>
-                {
-                    //Check if we have disposed between the event firing and the disposal of this class.
-                    //No need to dispose of the pipe server here because the dispose method on this class will handle that.
-                    if (isDisposed) return;
-                    
-                    //Create a new pipe before handling this one (to keep connection times down).
-                    CreateInstance();
+                pipeServer.OnConnect += PipeServer_OnConnect;
 
-                    //Adding the event listners here prevents the temporary pipe (the one waiting for a new connection) from firing events.
-                    pipeServer.OnMessage += (data) => OnMessage?.Invoke(guid, data);
-                    pipeServer.OnDispose += () => OnDispose?.Invoke(guid);
+                pendingPipeServer = pipeServer;
+            }
+        }
 
-                    //Fire the on-connection event for this new pipe.
-                    OnConnect?.Invoke(guid);
-                };
+        private void PipeServer_OnConnect()
+        {
+            //Check if we have disposed between the event firing and the disposal of this class.
+            //No need to dispose of the pipe server here because the dispose method on this class will handle that.
+            if (isDisposed) return;
 
-                //Ideally I should store this new pipe in a seperate variable until it gets a connection.
-                if (!pipeServers.TryAdd(guid, pipeServer))
-                {
-                    pipeServer.Dispose();
-                    throw new Exception("Unable to add new pipe server.");
-                }
+            //Generate a unique ID for the new pipe.
+            Guid guid;
+            do guid = Guid.NewGuid();
+            while (pipeServers.ContainsKey(guid));
+
+            PipeServer pipeServer = pendingPipeServer!;
+            if (!pipeServers.TryAdd(guid, pipeServer))
+            {
+                pipeServer.Dispose();
+                throw new Exception("Unable to add new pipe server.");
+            }
+            pendingPipeServer = null;
+
+            //Create a new pipe before handling this one (to keep connection times down).
+            try { CreateInstance(); }
+            catch (IOException) { }
+
+            //Adding the event listners here prevents the temporary pipe (the one waiting for a new connection) from firing events.
+            pipeServer.OnMessage += (data) => OnMessage?.Invoke(guid, data);
+            pipeServer.OnDispose += () => PipeServer_OnDispose(guid);
+
+            //Fire the on-connection event for this new pipe.
+            OnConnect?.Invoke(guid);
+        }
+
+        private void PipeServer_OnDispose(Guid guid)
+        {
+            OnDispose?.Invoke(guid);
+
+            pipeServers.TryRemove(guid, out _);
+
+            if (isDisposed) return;
+            
+            if (pendingPipeServer == null)
+            {
+                try { CreateInstance(); }
+                catch (IOException) { }
             }
         }
 
